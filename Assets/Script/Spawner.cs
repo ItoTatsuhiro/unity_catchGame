bf8723e42bad6bf8fba67739bed4defe285cd924
ItoTatsuhiro <ito.3229.wo@gmail.com>
2023-08-05 17:21:46 +0900

    本日作業分(アイテムスクリプト準備まで)

---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private float _interval = 1.0f;     // アイテム生成間隔
+    private float _esapsed;             // 前回生成からの経過時間
+
+    
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
